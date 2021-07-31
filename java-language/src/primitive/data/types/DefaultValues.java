8312172e953ee3bc14dbd6df926e8b55c57c9505
joeldevel <christianjoel.developer@gmail.com>
2021-07-30 23:40:19 -0300

    fomatting

--- java-language/src/primitive/data/types/DefaultValues.java
+++ java-language/src/primitive/data/types/DefaultValues.java
@@ -1,6 +1,7 @@
 package primitive.data.types;
 
 public class DefaultValues {
+
     byte b;
     char c;
     int i;
@@ -15,16 +16,16 @@ class Main {
     public static void main(String[] args) {
 
         DefaultValues dv = new DefaultValues();
-        
+
         System.out.println("**** Show default values for primitive data types ****");
-        
-        System.out.println("byte: "+ dv.b);
-        System.out.println("char: "+ dv.c);
-        System.out.println("int: "+ dv.i);
-        System.out.println("long: "+ dv.l);
-        System.out.println("float: "+ dv.f);
-        System.out.println("double: "+ dv.d);
-        System.out.println("boolean: "+ dv.bool);
+
+        System.out.println("byte: " + dv.b);
+        System.out.println("char: " + dv.c);
+        System.out.println("int: " + dv.i);
+        System.out.println("long: " + dv.l);
+        System.out.println("float: " + dv.f);
+        System.out.println("double: " + dv.d);
+        System.out.println("boolean: " + dv.bool);
 
     }
 }
